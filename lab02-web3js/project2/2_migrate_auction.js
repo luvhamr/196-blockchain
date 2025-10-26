bffecf1d9f6773b10ce6495e283511665678b686
luvhamr <iam.euhn@gmail.com>
2025-10-25 20:11:48 -0700

    Create auction smart contract and migration script

---
+++ lab02-web3js/project2/2_migrate_auction.js
@@ -0,0 +1,8 @@
+onst SimpleAuction = artifacts.require("SimpleAuction");
+module.exports = async function (deployer) {
+    // Set the auction duration and the beneficiary address. 
+    const accounts = await web3.eth.getAccounts();
+    const biddingTime = 600; // Example: auction duration is 10 minutes (600 seconds)
+    const beneficiaryAddress = accounts[0]; // Example: beneficiary is the deployer
+    await deployer.deploy(SimpleAuction, biddingTime, beneficiaryAddress,);
+};
